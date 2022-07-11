31acb23d9c22e32f1ccb99df6b2b742a400e6ddc
Mariia Heiskanen <85872157+Hmariia@users.noreply.github.com>
2022-07-11 09:15:00 -0700

    Update Issuesbar.js

--- src/components/Issuesbar.js
+++ src/components/Issuesbar.js
@@ -6,8 +6,8 @@ const Issuesbar = ({ recentIssues }) => {
       <h3>Issues ({ recentIssues?.length })</h3>
       <div className="issues-content">
         {recentIssues?.map((issue, index) => issue === 1 ? <div key={index}>deep discharge</div> : 
-        issue === 2 ? <div>overheating</div> : issue === 3 ? <div>unknown anomaly</div> :
-        issue === 4 && <div>missing data</div> ) }
+        issue === 2 ? <div key={index}>overheating</div> : issue === 3 ? <div key={index}>unknown anomaly</div> :
+        issue === 4 && <div key={index}>missing data</div> ) }
       </div>
     </div>
   )
